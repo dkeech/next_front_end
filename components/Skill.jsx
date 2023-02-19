b48dbc544aba138184799123e1612508d5d0e49a
Dan <ddkeech@gmail.com>
2023-02-19 15:33:50 -0500

    relative image fix

--- components/Skill.jsx
+++ components/Skill.jsx
@@ -10,7 +10,7 @@ const Skill = (props) => {
                 <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
                     <div className="flex flex-col justify-center items-center">
                         <div className="m-auto">
-                            <Image src={`/../public/assets/skills/${img}.png`} alt={title} width={100} height={100}/>
+                            <Image src={`/assets/skills/${img}.png`} alt={title} width={100} height={100}/>
                         </div>
                         <div className="flex flex-col items-center justify-center">
                             <h3>{title}</h3>
