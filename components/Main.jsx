9b8389b04f6c2a9f4bec8cc20e0308032bea1ea6
Dan <ddkeech@gmail.com>
2023-02-19 15:18:45 -0500

    title change

--- components/Main.jsx
+++ components/Main.jsx
@@ -12,7 +12,7 @@ const Main = () => {
             Let&apos;s build some things together!
           </p>
           <h1 className="py-4 text-gray-700">Hi, I&apos;m Dan</h1>
-          <h1 className="py-2 text-gray-500">A Front-End Web Developer</h1>
+          <h1 className="py-2 text-gray-500">I Am a Front-End Web Developer</h1>
           <p className="py-4 text-gray-600">
             I specialize in building beautiful, responsive, accessible,
             high-performing websites in WordPress.
