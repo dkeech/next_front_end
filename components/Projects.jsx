9629c5265e42bda366078d4f4e5863e153f1e683
Dan Keech <ddkeech@gmail.com>
2024-02-10 14:05:05 -0500

    added style to make project cards wider

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Project from "./Project";
 
-const siteBase = "https://dankeech.com"
+const siteBase = "https://dankeech.com";
 const baseURL = `${siteBase}/wp-json/wp/v2/projects/`;
 
 export default function Projects() {
@@ -22,22 +22,27 @@ export default function Projects() {
 
   if (isLoaded) {
     return (
-      <div id="projects" className="max-w-xl w-full p-8 m-auto flex flex-col justify-center items-center py-16 ">
-      <h2 className="pt-[200px]">Projects</h2>
-        {projects.slice(0,6).filter(project => {
-          return project.acf.display_order < 51
-        }).map((project) => (
-          <Project 
-            key={project.id} 
-            title={project.title.rendered} 
-            image={project.acf.main_image}
-            content={project.content.rendered} />
-
-      ))}
-    </div>
-    )
+      <div
+        id="projects"
+        className="project-card max-w-xl w-full p-8 m-auto flex flex-col justify-center items-center py-16 "
+      >
+        <h2 className="pt-[200px]">Projects</h2>
+        {projects
+          .slice(0, 6)
+          .filter((project) => {
+            return project.acf.display_order < 51;
+          })
+          .map((project) => (
+            <Project
+              key={project.id}
+              title={project.title.rendered}
+              image={project.acf.main_image}
+              content={project.content.rendered}
+            />
+          ))}
+      </div>
+    );
   } else {
     return <h2 className="text-center py-16">Projects Loading...</h2>;
   }
 }
-
