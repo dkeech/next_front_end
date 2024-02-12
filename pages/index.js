1eb04dc5ed6af967b8378dc4de5df54070e3a1b6
Dan Keech <ddkeech@gmail.com>
2024-02-12 18:03:37 -0500

    viewport width for mobile devices

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,18 @@ import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import Education from "../components/Education";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Dan Keech | Web Developer</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Dan Keech's portfolio" />
+        <title>Dan Keech | Web Developer</title>
+
         <link rel="icon" href="/assets/favicon.ico" />
-        
       </Head>
       <Navbar />
       <Main />
